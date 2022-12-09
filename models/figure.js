69fa85cfc3cefce47b92ca38957adfc8795f8bee
Jamie Vickers <psyjv3@exmail.nottingham.ac.uk>
2022-12-09 18:44:11 +0000

    Fix mongoose.connect call

--- models/figure.js
+++ models/figure.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-await mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI);
 
 const figureSchema = new mongoose.Schema({
 	imageID: {
