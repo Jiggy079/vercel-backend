b4583153dfd5b02892e5d7334881d3cd660f0f0c
Jamie Vickers <psyjv3@exmail.nottingham.ac.uk>
2022-12-09 18:50:25 +0000

    Change to callback query

--- routes/api/figures.js
+++ routes/api/figures.js
@@ -5,12 +5,11 @@ const r = Router();
 
 r.get("/figures/:index", async function (req, res) {
 	try {
-		const data = await Figure.find({imageId: req.params.index}).exec();
+		const data = await Figure.find({imageId: req.params.index});
 		res.json(data);
 	} catch (error) {
 		res.status(500).json({message: error.message});
 	}
-
 });
 
 module.exports = r;
