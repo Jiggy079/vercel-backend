bd5b8f16a9591c85eec5372b2e7b72a89a16c29e
Jamie Vickers <psyjv3@exmail.nottingham.ac.uk>
2022-12-10 17:39:50 +0000

    Remove imageid from json response

--- routes/api/figures.js
+++ routes/api/figures.js
@@ -7,7 +7,7 @@ const r = Router();
 r.get("/figures/:index", async function (req, res) {
 	try {
 		const data = await Figure.find({imageId: parseInt(req.params.index)});
-		res.json(new SuccessResponseObject(" ", [data["imageID"], data["name"], data["url"], data["doi"], data["year"]]));
+		res.json(new SuccessResponseObject(" ", [data["name"], data["url"], data["doi"], data["year"]]));
 	} catch (error) {
 		res.status(500).json({message: error.message});
 	}
