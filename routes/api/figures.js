4dd17d4b6e0c14c2e5360dc12e033bd7d3283321
Jamie Vickers <psyjv3@exmail.nottingham.ac.uk>
2022-12-10 17:42:25 +0000

    Change case of search query

--- routes/api/figures.js
+++ routes/api/figures.js
@@ -6,8 +6,8 @@ const r = Router();
 
 r.get("/figures/:index", async function (req, res) {
 	try {
-		const data = await Figure.find({imageId: parseInt(req.params.index)});
-		res.json(new SuccessResponseObject(" ", [data["name"], data["url"], data["doi"], data["year"]]));
+		const data = await Figure.find({imageID: parseInt(req.params.index)});
+		res.json(new SuccessResponseObject(req.params.index, [data["name"], data["url"], data["doi"], data["year"]]));
 	} catch (error) {
 		res.status(500).json({message: error.message});
 	}
