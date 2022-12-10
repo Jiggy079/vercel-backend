0c24cb48c75599c6d52f1e627be6062b16dcf2d5
Jamie Vickers <psyjv3@exmail.nottingham.ac.uk>
2022-12-10 16:14:22 +0000

    Test json response

--- routes/api/figures.js
+++ routes/api/figures.js
@@ -7,7 +7,7 @@ const r = Router();
 r.get("/figures/:index", async function (req, res) {
 	try {
 		const data = await Figure.find({imageId: parseInt(req.params.index)});
-		res.json(new SuccessResponseObject(" ", data.json()));
+		res.json(new SuccessResponseObject(" ", data));
 	} catch (error) {
 		res.status(500).json({message: error.message});
 	}
