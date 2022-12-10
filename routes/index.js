733bc6033196afe2dfe950c345a7c90ffc30b30b
Jamie Vickers <psyjv3@exmail.nottingham.ac.uk>
2022-12-10 19:49:26 +0000

    Add /annotations routing

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ const { Router } = require('express');
 const { SuccessResponseObject } = require('../common/http');
 const demo = require('./demo.route');
 const figures = require("./api/figures");
+const annotations = require("./api/annotations");
 
 const r = Router();
 
 r.use('/demo', demo);
 r.use("/api", figures);
+r.use("/api", annotations);
 
 r.get('/', (req, res) => res.json(new SuccessResponseObject('express vercel boiler plate')));
 
