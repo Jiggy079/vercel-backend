a56e71c94da9d663d0172645e6b08d5fb782cfc5
Jamie Vickers <psyjv3@exmail.nottingham.ac.uk>
2022-12-10 20:21:31 +0000

    Change to using doc.save() method

--- routes/api/annotations.js
+++ routes/api/annotations.js
@@ -16,10 +16,16 @@ r.get("/annotations/:index", async function (req, res) {
 
 r.post("/annotations/:index", async function (req, res) {
 	try {
-		const filter = {imageID: parseInt(req.params.index)};
 		const newData = req.body;
-		const doc = await Annotation.findOneAndUpdate(filter, newData, {new: true});
-		res.json(new SuccessResponseObject("sucessfully updated", doc));
+		const data = await Annotation.find({imageID: parseInt(req.params.index)});
+		data.type = newData["type"];
+		data.usage = newData["usage"];
+		data.legend = newData["legend"];
+		data.mappingtype = newData["mappingtype"];
+		data.amount = newData["amount"];
+		data.difficulty = newData["difficulty"];
+		await data.save();
+		res.json(new SuccessResponseObject("sucessfully updated", data));
 	} catch (error) {
 		res.status(500).json({message: error.message});
 	}
