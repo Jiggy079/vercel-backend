6773f9ceeeaf594d9802c7566c5ca24257688853
Jamie Vickers <psyjv3@exmail.nottingham.ac.uk>
2022-12-12 18:29:07 +0000

    Allow all CORS origins

--- app.js
+++ app.js
@@ -9,9 +9,7 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(helmet());
-app.use(cors({
-	origin: "https://testing-vercel-dfc9.vercel.app/"
-}));
+app.use(cors());
 app.use('/', routes);
 
 // default catch all handler
